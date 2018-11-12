cb0f72a9f9ea793009396f5ef7179e95281bd00f
Liza <ev2154@gmail.com>
2018-11-11 19:30:35 -0500

    adjust modal styling and add animations to modal entry and exit, fix tabbing in scss files

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -4,10 +4,11 @@ import { EVENTS } from '../../core/constants/events';
 
 export class Modal {
 	constructor() {
+		this.mainContent = document.querySelector('.content-wrapper');
 		this.modal = document.getElementById('modal');
-		this.overlay = this.modal.querySelector('.modal-overlay');
+		this.overlay = document.querySelector('.modal-overlay');
 		this.closeBtn = this.modal.querySelector('.modal-close-btn');
-		this.img = this.modal.querySelector('img');
+		this.img = this.modal.querySelector('img.main-img');
 
 		this.overlay.addEventListener('click', () => this.close());
 		this.closeBtn.addEventListener('click', () => this.close());
@@ -17,9 +18,11 @@ export class Modal {
 
 	close() {
 		this.modal.classList.remove('open');
+		this.mainContent.classList.remove('modal-open');
 	}
 
 	open(url) {
+		this.mainContent.classList.add('modal-open');
 		this.img.src = url;
 		this.modal.classList.add('open');
 	}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './core/styles/global.scss';
 import { Gallery } from './components/Gallery/gallery';
-// import { Modal } from './components/Modal/modal';
+import { Modal } from './components/Modal/modal';
 
 document.addEventListener('DOMContentLoaded', () => {
 	const gallery = new Gallery();
-	// const modal = new Modal();
+	// eslint-disable-next-line no-unused-vars
+	const modal = new Modal();
 
 	gallery.load();
 
